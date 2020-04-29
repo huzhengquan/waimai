6fb2aecf3778da16b6e33b8bdd128f50f910f633
hu <hzq@esc.local>
2020-04-29 21:13:35 +0800

    null

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/huzhengquan/waimai"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.json "0.2.7"]
                  [digest "1.4.9"]
                  [http-kit "2.3.0"]]
